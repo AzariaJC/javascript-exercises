e994d883a136b5b13c4d8bcbaf4548b06cc6cb67
AzariaJC <45110670+AzariaJC@users.noreply.github.com>
2025-07-21 16:03:28 -0400

    all tests for ex 12 run

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,37 @@
-const add = function(a, b) {
-	let sum = (a + b);
+const add = function (a, b) {
+  let sum = a + b;
   return sum;
 };
 
-const subtract = function(a, b) {
-	let difference = (a - b);
+const subtract = function (a, b) {
+  let difference = a - b;
   return difference;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sumation = array.reduce((item, currentItem) => item + currentItem, 0);
+  return sumation;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let product = array.reduce((item, currentItem) => item * currentItem, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let resultantOfPower = (a ** b);
+  return resultantOfPower;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a < 0) {
+    return "does not exist";
+  }
+  let factorialMultiplier = 1
+  for (let i = 1; i <= a; i++) {
+    factorialMultiplier *= i
+  }
+  return factorialMultiplier;
 };
 
 // Do not edit below this line
@@ -31,5 +41,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
